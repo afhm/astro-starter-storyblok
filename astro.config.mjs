861f2e73151aa26e9ff73d3d7fa2acec991ea6f0
Afham <afh.a@hotmail.com>
2023-10-09 14:11:05 +0530

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,9 +9,14 @@ import { ANALYTICS, SITE } from './src/utils/config';
 import { readingTimeRemarkPlugin } from './src/utils/frontmatter.mjs';
 import tasks from './src/utils/tasks.mjs';
 import storyblok from '@storyblok/astro';
-import solidJs from "@astrojs/solid-js";
+import solidJs from '@astrojs/solid-js';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const whenExternalScripts = (items = []) => ANALYTICS.vendors.googleAnalytics.id && ANALYTICS.vendors.googleAnalytics.partytown ? Array.isArray(items) ? items.map(item => item()) : [items()] : [];
+const whenExternalScripts = (items = []) =>
+  ANALYTICS.vendors.googleAnalytics.id && ANALYTICS.vendors.googleAnalytics.partytown
+    ? Array.isArray(items)
+      ? items.map((item) => item())
+      : [items()]
+    : [];
 
 // https://astro.build/config
 export default defineConfig({
@@ -19,38 +24,48 @@ export default defineConfig({
   base: SITE.base,
   trailingSlash: SITE.trailingSlash ? 'always' : 'never',
   output: 'static',
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), storyblok({
-    accessToken: process.env.STORYBLOK_TOKEN,
-    bridge: process.env.SITE_NAME === 'manuelschroeder' ? false : true,
-    components: {
-      page: 'storyblok/Page',
-      hero: 'components/Hero',
-      header: 'components/Header',
-    }
-  }), sitemap(), ...whenExternalScripts(() => partytown({
-    config: {
-      forward: ['dataLayer.push']
-    }
-  })), tasks(), compress({
-    CSS: true,
-    HTML: {
-      removeAttributeQuotes: false
-    },
-    Image: false,
-    JavaScript: true,
-    SVG: true,
-    Logger: 1
-  }), solidJs()],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    storyblok({
+      accessToken: process.env.STORYBLOK_TOKEN,
+      bridge: process.env.SITE_NAME === 'manuelschroeder' ? false : true,
+      components: {
+        page: 'storyblok/Page',
+        hero: 'components/Hero',
+        header: 'components/Header',
+      },
+    }),
+    sitemap(),
+    ...whenExternalScripts(() =>
+      partytown({
+        config: {
+          forward: ['dataLayer.push'],
+        },
+      })
+    ),
+    tasks(),
+    compress({
+      CSS: true,
+      HTML: {
+        removeAttributeQuotes: false,
+      },
+      Image: false,
+      JavaScript: true,
+      SVG: true,
+      Logger: 1,
+    }),
+    solidJs(),
+  ],
   markdown: {
-    remarkPlugins: [readingTimeRemarkPlugin]
+    remarkPlugins: [readingTimeRemarkPlugin],
   },
   vite: {
     resolve: {
       alias: {
-        '~': path.resolve(__dirname, './src')
-      }
-    }
-  }
+        '~': path.resolve(__dirname, './src'),
+      },
+    },
+  },
 });
